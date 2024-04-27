2bf6850c36af4c77d5e891f48b1b8a54a81ee3d9
Guillem Alqueza Mayench <guillem.alqueza@estudiantat.upc.edu>
2024-04-27 13:26:01 +0200

    optimize code

--- Project II/Game/Source/Puzzle.cpp
+++ Project II/Game/Source/Puzzle.cpp
@@ -27,18 +27,18 @@ bool Puzzle::Awake() {
 bool Puzzle::Start() {
 
 	//initilize texture
-	texture1 = app->tex->Load("Assets/Textures/pieza_puzle_1.png");
-	texture2 = app->tex->Load("Assets/Textures/pieza_puzle_2.png");
-	texture3 = app->tex->Load("Assets/Textures/pieza_puzle_3.png");
-	texture4 = app->tex->Load("Assets/Textures/pieza_puzle_4.png");
+	texture[0] = app->tex->Load("Assets/Textures/pieza_puzle_1.png");
+	texture[1] = app->tex->Load("Assets/Textures/pieza_puzle_2.png");
+	texture[2] = app->tex->Load("Assets/Textures/pieza_puzle_3.png");
+	texture[3] = app->tex->Load("Assets/Textures/pieza_puzle_4.png");
 	selectedTexture = app->tex->Load("Assets/Textures/selected_piece.png");
 	backgroundTexture = app->tex->Load("Assets/Textures/puzzle_background.png");
 	backgroundTexture2 = app->tex->Load("Assets/Textures/puzzle_background2.png");
 
-	piece1Pos = piece1InitialPos;
-	piece2Pos = piece2InitialPos;
-	piece3Pos = piece3InitialPos;
-	piece4Pos = piece4InitialPos;
+	for (int i = 0; i < 4; i++)
+	{
+		piecePos[i] = pieceInitialPos[i];
+	}
 
 	return true;
 }
@@ -47,86 +47,31 @@ bool Puzzle::Update(float dt)
 {
 	if (pieceCollected[0] && pieceCollected[1] && pieceCollected[2] && pieceCollected[3])
 	{
-
-		if (app->input->GetKey(SDL_SCANCODE_1) == KEY_DOWN)
+		for (int i = 0; i < 4; ++i)
 		{
-			if (!isSelecting && !pieceInSlot[0])
+			if (app->input->GetKey(SDL_SCANCODE_1 + i) == KEY_DOWN)
 			{
-				selectedPiece = 0;
-				isSelecting = true;
-				pieceInSlot[0] = true;
-			}
-			else if (isSelecting && !slotOccupied[0])
-			{
-				slotOccupied[0] = true;
-				if (selectedPiece == 0) piece1Pos = slot1Pos;
-				else if (selectedPiece == 1) piece2Pos = slot1Pos;
-				else if (selectedPiece == 2) piece3Pos = slot1Pos;
-				else if (selectedPiece == 3) piece4Pos = slot1Pos;
-				isSelecting = false;
-			}
-		}
-		if (app->input->GetKey(SDL_SCANCODE_2) == KEY_DOWN)
-		{
-			if (!isSelecting && !pieceInSlot[1])
-			{
-				selectedPiece = 1;
-				isSelecting = true;
-				pieceInSlot[1] = true;
-			}
-			else if (isSelecting && !slotOccupied[1])
-			{
-				slotOccupied[1] = true;
-				if (selectedPiece == 0) piece1Pos = slot2Pos;
-				else if (selectedPiece == 1) piece2Pos = slot2Pos;
-				else if (selectedPiece == 2) piece3Pos = slot2Pos;
-				else if (selectedPiece == 3) piece4Pos = slot2Pos;
-				isSelecting = false;
-			}
-		}
-		if (app->input->GetKey(SDL_SCANCODE_3) == KEY_DOWN)
-		{
-			if (!isSelecting && !pieceInSlot[2])
-			{
-				selectedPiece = 2;
-				isSelecting = true;
-				pieceInSlot[2] = true;
-			}
-			else if (isSelecting && !slotOccupied[2])
-			{
-				slotOccupied[2] = true;
-				if (selectedPiece == 0) piece1Pos = slot3Pos;
-				else if (selectedPiece == 1) piece2Pos = slot3Pos;
-				else if (selectedPiece == 2) piece3Pos = slot3Pos;
-				else if (selectedPiece == 3) piece4Pos = slot3Pos;
-				isSelecting = false;
-			}
-		}
-		if (app->input->GetKey(SDL_SCANCODE_4) == KEY_DOWN)
-		{
-			if (!isSelecting && !pieceInSlot[3])
-			{
-				selectedPiece = 3;
-				isSelecting = true;
-				pieceInSlot[3] = true;
-			}
-			else if (isSelecting && !slotOccupied[3])
-			{
-				slotOccupied[3] = true;
-				if (selectedPiece == 0) piece1Pos = slot4Pos;
-				else if (selectedPiece == 1) piece2Pos = slot4Pos;
-				else if (selectedPiece == 2) piece3Pos = slot4Pos;
-				else if (selectedPiece == 3) piece4Pos = slot4Pos;
-				isSelecting = false;
+				if (!isSelecting && !pieceInSlot[i])
+				{
+					selectedPiece = i;
+					isSelecting = true;
+					pieceInSlot[i] = true;
+				}
+				else if (isSelecting && !slotOccupied[i])
+				{
+					slotOccupied[i] = true;
+					piecePos[selectedPiece] = slotPos[i];
+					isSelecting = false;
+				}
 			}
 		}
 
 		if (app->input->GetKey(SDL_SCANCODE_R) == KEY_DOWN)
 		{
-			piece1Pos = piece1InitialPos;
-			piece2Pos = piece2InitialPos;
-			piece3Pos = piece3InitialPos;
-			piece4Pos = piece4InitialPos;
+			for (int i = 0; i < 4; i++)
+			{
+				piecePos[i] = pieceInitialPos[i];
+			}
 			for (int i = 0; i < 4; i++)
 			{
 				slotOccupied[i] = false;
@@ -149,25 +94,27 @@ void Puzzle::DrawPieces()
 {
 	app->render->DrawTexture(backgroundTexture, 530, 180, NULL, SDL_FLIP_NONE, 0.0f);
 	app->render->DrawTexture(backgroundTexture2, 430, 460, NULL, SDL_FLIP_NONE, 0.0f);
-	if (pieceCollected[0]) app->render->DrawTexture(texture1, piece1Pos.x, piece1Pos.y, NULL, SDL_FLIP_NONE, 0.0f);
-	if (pieceCollected[1]) app->render->DrawTexture(texture2, piece2Pos.x, piece2Pos.y, NULL, SDL_FLIP_NONE, 0.0f);
-	if (pieceCollected[2]) app->render->DrawTexture(texture3, piece3Pos.x, piece3Pos.y, NULL, SDL_FLIP_NONE, 0.0f);
-	if (pieceCollected[3]) app->render->DrawTexture(texture4, piece4Pos.x, piece4Pos.y, NULL, SDL_FLIP_NONE, 0.0f);
-	if (isSelecting)
+	for (int i = 0; i < 4; ++i)
 	{
-		if (selectedPiece == 0) app->render->DrawTexture(selectedTexture, piece1InitialPos.x, piece1InitialPos.y, NULL, SDL_FLIP_NONE, 0.0f);
-		else if (selectedPiece == 1) app->render->DrawTexture(selectedTexture, piece2InitialPos.x, piece2InitialPos.y, NULL, SDL_FLIP_NONE, 0.0f);
-		else if (selectedPiece == 2) app->render->DrawTexture(selectedTexture, piece3InitialPos.x, piece3InitialPos.y, NULL, SDL_FLIP_NONE, 0.0f);
-		else if (selectedPiece == 3) app->render->DrawTexture(selectedTexture, piece4InitialPos.x, piece4InitialPos.y, NULL, SDL_FLIP_NONE, 0.0f);
+		if (pieceCollected[i])
+		{
+			app->render->DrawTexture(texture[i], piecePos[i].x, piecePos[i].y, NULL, SDL_FLIP_NONE, 0.0f);
+		}
 	}
+
+	if (isSelecting && selectedPiece >= 0 && selectedPiece < 4)
+	{
+		app->render->DrawTexture(selectedTexture, pieceInitialPos[selectedPiece].x, pieceInitialPos[selectedPiece].y, NULL, SDL_FLIP_NONE, 0.0f);
+	}
+
 }
 
 bool Puzzle::CleanUp()
 {
-	app->tex->UnLoad(texture1);
-	app->tex->UnLoad(texture2);
-	app->tex->UnLoad(texture3);
-	app->tex->UnLoad(texture4);
+	app->tex->UnLoad(texture[0]);
+	app->tex->UnLoad(texture[1]);
+	app->tex->UnLoad(texture[2]);
+	app->tex->UnLoad(texture[3]);
 
 	return true;
 }
--- Project II/Game/Source/Puzzle.h
+++ Project II/Game/Source/Puzzle.h
@@ -29,10 +29,8 @@ public:
 
 public:
 
-	SDL_Texture* texture1 = NULL;
-	SDL_Texture* texture2 = NULL;
-	SDL_Texture* texture3 = NULL;
-	SDL_Texture* texture4 = NULL;
+	SDL_Texture* texture[4] = { NULL,NULL,NULL,NULL };
+
 	SDL_Texture* selectedTexture = NULL;
 	SDL_Texture* backgroundTexture = NULL;
 	SDL_Texture* backgroundTexture2 = NULL;
@@ -46,20 +44,11 @@ public:
 	float dt;
 	bool showUI = false;
 
-	iPoint slot1Pos = { 550, 200 };
-	iPoint slot2Pos = { 650, 200 };
-	iPoint slot3Pos = { 550, 300 };
-	iPoint slot4Pos = { 650, 300 };
+	iPoint slotPos[4] = { {550, 200},{ 650, 200 },{ 550, 300 },{ 650, 300 } };
 
-	iPoint piece1InitialPos = { 455, 485 };
-	iPoint piece2InitialPos = { 550, 485 };
-	iPoint piece3InitialPos = { 645, 485 };
-	iPoint piece4InitialPos = { 740, 485 };
+	iPoint pieceInitialPos[4] = { {455, 485},{ 550, 485 },{ 645, 485 },{ 740, 485 } };
 
-	iPoint piece1Pos = {0,0};
-	iPoint piece2Pos = {0,0};
-	iPoint piece3Pos = {0,0};
-	iPoint piece4Pos = {0,0};
+	iPoint piecePos[4] = {{0,0},{0,0},{0,0},{0,0}};
 
 };
 
