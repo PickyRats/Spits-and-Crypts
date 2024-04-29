0ba97667b79cbc085ac930617550fa9229050c53
MiguelIglesiasAbarca <125640186+MiguelIglesiasAbarca@users.noreply.github.com>
2024-04-29 20:38:53 +0200

    Floor1 fixes + more implementations

--- Project II/Game/Source/SceneFloor1.h
+++ Project II/Game/Source/SceneFloor1.h
@@ -61,8 +61,8 @@ public:
 	bool win = false;
 	int sceneNum = 1;
 
-	int levelWidth = 37 * 64;
-	int levelHeight = 12 * 64;
+	int levelWidth = 200 * 64;
+	int levelHeight = 37 * 64;
 
 private:
 	SDL_Texture* backgroundTexture;
